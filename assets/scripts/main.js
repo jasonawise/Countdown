e6e53c88b03e4f710eb7b947868a20a09eec78f6
Jason A. Wise <jasonawise@icloud.com>
2017-11-18 17:27:32 -0500

    have the countdown working

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
     console.log("ready!");
 
-    //get display element
-    const countdown = $('.displayCountdown');
+    var x = setInterval(function() {
+        //get display element
+        const countdown = $('.displayCountdown');
+
+        //set the date we are counting down to
+        const countDownDate = new Date("Jan 5, 2018").getTime();
+        //get todays date
+        const now = new Date().getTime();
+
+        // Find the distance between now an the count down date
+        const distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        //Display the result
+        countdown.text(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+
+    }, 1000);
 
 });
